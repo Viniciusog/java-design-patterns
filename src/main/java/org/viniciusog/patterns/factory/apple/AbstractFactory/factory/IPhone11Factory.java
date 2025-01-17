c7588965a572b59eeae2b65e10d43123c939aa0a
Vinícius Guimarães <vinijipa23@gmail.com>
2025-01-16 23:55:15 -0300

    Add Factories and verification for HighEnd and Standard levels

--- src/main/java/org/viniciusog/patterns/factory/apple/AbstractFactory/factory/IPhone11Factory.java
+++ src/main/java/org/viniciusog/patterns/factory/apple/AbstractFactory/factory/IPhone11Factory.java
@@ -2,21 +2,21 @@ package org.viniciusog.patterns.factory.apple.AbstractFactory.factory;
 
 import org.viniciusog.patterns.factory.apple.AbstractFactory.factory.abstractFactory.CountryRulesAbstractFactory;
 import org.viniciusog.patterns.factory.apple.AbstractFactory.model.iphone.IPhone;
-import org.viniciusog.patterns.factory.apple.AbstractFactory.model.iphone.IPhoneX;
-import org.viniciusog.patterns.factory.apple.AbstractFactory.model.iphone.IPhoneXSMax;
+import org.viniciusog.patterns.factory.apple.AbstractFactory.model.iphone.IPhone11;
+import org.viniciusog.patterns.factory.apple.AbstractFactory.model.iphone.IPhone11Pro;
 
-public class IPhoneXFactory extends IPhoneFactory {
+public class IPhone11Factory extends IPhoneFactory {
 
-    public IPhoneXFactory(CountryRulesAbstractFactory rules) {
+    public IPhone11Factory(CountryRulesAbstractFactory rules) {
         super(rules);
     }
 
     @Override
     protected IPhone createIphone(String level) {
         if ("standard".equals(level)) {
-            return new IPhoneX(this.rules);
+            return new IPhone11(this.rules);
         } else if ("highEnd".equals(level)) {
-            return new IPhoneXSMax(this.rules);
+            return new IPhone11Pro(this.rules);
         }
         return null;
     }
